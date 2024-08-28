ae6144334dfac20edb511944fa2a63de62250f53
Luiz Felipe Santos De Abreu <126472482+OGUTAO@users.noreply.github.com>
2024-08-28 22:01:48 +0000

    curso.py

--- Basico/identacaoBlocos.py
+++ Basico/identacaoBlocos.py
@@ -1,38 +1,16 @@
-def sacar(saldo):
-    print("Seu saldo é de:",saldo, "reais deseja fazer algum saque? ")
-    quero = int(input("Digite 1 para SIM ou 0 para NÃO: "))
+# Método sem pedir o valor para o cliente (eu já defini os valores lá embaixo quando chamei a função)
+def sacando(valorsaque):
+    conta = 500
 
-    if quero == 1:
-        valor = float(input("Qual valor deseja sacar: ")) 
-        if saldo >= valor:  
-            saldo_restante = saldo - valor
-            print("Valor sacado!") 
-            print("Saldo restante: ", saldo_restante)
-            return saldo_restante
-        else:
-            print("Saldo insuficiente!")
-            return saldo
-    else:
-        print("Operação cancelada!")
-        return saldo
+    if conta >= valorsaque:
+        print("valor sacado!")
+        print("retire o seu dinheiro na boca do caixa")
 
-    
-def depositar(saldo):
-    depositado = float(input("Deseja depositar algum valor? Digite 1 para SIM ou 0 para NÃO: "))
+print("Valor não sacado; valor mais alto do que você possui, obrigado por ser nosso cliente, tenha um bom dia!")
 
-    if depositado == 1:
-        quero2 = float(input("Qual o valor? "))
-        saldo_final = saldo + quero2
-        print("Valor depositado!")
-        print("Seu saldo Final é: ", saldo_final)
-        return saldo_final
-    else: 
-        print("Finalizando operação, seu saldo na conta é de", saldo)
-        return saldo
 
-saldo = 500
-saldo = sacar(saldo) 
-saldo = depositar(saldo)
+def deposito(valorsaque):
+    conta = 500
+    conta += valorsaque
 
-    
-
+valorsaque = sacando(100) # Dependendo do valor colocado aqui ele não vai sacar pois é maior que o valor da conta
