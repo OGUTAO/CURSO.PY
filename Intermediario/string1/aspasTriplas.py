ad8a5cfa0fdecebcf9912f8120026863c11ca09c
Luiz Felipe Santos De Abreu <126472482+OGUTAO@users.noreply.github.com>
2024-09-06 00:42:50 +0000

    curso.py

---
+++ Intermediario/string1/aspasTriplas.py
@@ -0,0 +1,23 @@
+nome = "Gutão"
+
+mensagem = f"""
+    Olá meu nome é {nome},
+eu estou aprendendo Python
+    Essa mensagem tem diferentes recuos
+"""
+
+print(mensagem)
+
+
+print(
+    """ 
+    ######## MENU #########
+
+    1 - DEPOSITAR
+    2 - SACAR 
+    0 - SAIR 
+
+    #######################
+
+            Obrigado por usar nosso sistema!!!
+""")
